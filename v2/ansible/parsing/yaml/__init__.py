48c160128b852e354a5dcc47214d588d09f20dcb
James Cammarata <jimi@sngx.net>
2014-10-23 15:39:27 -0500

    Adding a data parsing class for v2

--- v2/ansible/parsing/yaml/__init__.py
+++ v2/ansible/parsing/yaml/__init__.py
@@ -19,9 +19,82 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from yaml import load
-from ansible.parsing.yaml.loader import AnsibleLoader
+import json
+import os
 
-def safe_load(stream):
-    ''' implements yaml.safe_load(), except using our custom loader class '''
-    return load(stream, AnsibleLoader)
+from yaml import load, YAMLError
+
+from ansible.errors import AnsibleParserError
+
+from ansible.parsing.vault import VaultLib
+from ansible.parsing.yaml.loader import AnsibleLoader
+from ansible.parsing.yaml.objects import AnsibleBaseYAMLObject
+from ansible.parsing.yaml.strings import YAML_SYNTAX_ERROR
+
+class DataLoader():
+
+    _FILE_CACHE = dict()
+
+    def __init__(self, vault_password=None):
+        if vault_password:
+            self._vault = VaultLib(password=vault_password)
+        else:
+            self._vault = None
+
+    def load(self, data, file_name='<string>', show_content=True):
+        try:
+            # we first try to load this data as JSON
+            return json.loads(data)
+        except:
+            try:
+                # if loading JSON failed for any reason, we go ahead
+                # and try to parse it as YAML instead
+                return self._safe_load(data)
+            except YAMLError, yaml_exc:
+                self._handle_error(yaml_exc, file_name, show_content)
+
+    def load_from_file(self, file_name):
+        # if the file has already been read in and cached, we'll
+        # return those results to avoid more file/vault operations
+        if file_name in self._FILE_CACHE:
+            return self._FILE_CACHE
+
+        # read the file contents and load the data structure from them
+        (file_data, show_content) = self._get_file_contents(file_name)
+        parsed_data = self.load(data=file_data, file_name=file_name, show_content=show_content)
+
+        # cache the file contents for next time
+        self._FILE_CACHE[file_name] = parsed_data
+
+        return parsed_data
+
+    def _safe_load(self, stream):
+        ''' implements yaml.safe_load(), except using our custom loader class '''
+        return load(stream, AnsibleLoader)
+
+    def _get_file_contents(self, file_name):
+        if not os.path.exists(file_name) or not os.path.isfile(file_name):
+            raise AnsibleParserError("the file_name '%s' does not exist, or is not readable" % file_name)
+
+        show_content = True
+        try:
+            with open(file_name, 'r') as f:
+                data = f.read()
+                if self._vault.is_encrypted(data):
+                    data = self._vault.decrypt(data)
+                    show_content = False
+            return (data, show_content)
+        except (IOError, OSError) as e:
+            raise AnsibleParserError("an error occured while trying to read the file '%s': %s" % (file_name, str(e)))
+
+    def _handle_error(self, yaml_exc, file_name, show_content):
+
+        # if the YAML exception contains a problem mark, use it to construct
+        # an object the error class can use to display the faulty line
+        err_obj = None
+        if hasattr(yaml_exc, 'problem_mark'):
+            err_obj = AnsibleBaseYAMLObject()
+            err_obj.set_position_info(file_name, yaml_exc.problem_mark.line + 1, yaml_exc.problem_mark.column + 1)
+
+        raise AnsibleParserError(YAML_SYNTAX_ERROR, obj=err_obj, show_content=show_content)
+
